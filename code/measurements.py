0511c8f23947d1a5ef40f18aabba0376ba4a2830
DaminK <damin.kuehn@rwth-aachen.de>
2021-08-05 17:52:54 +0200

    Factor out some measurements + some testing for filtering for different conditions

---
+++ code/measurements.py
@@ -0,0 +1,33 @@
+
+import numpy as np
+from enum import Enum
+from data import SVDData #,Comp Superclass
+
+#functions expert temps in shape of trials x frames x comp with frames being already filtered to same length
+
+#instead of temps, just access temps from comp super class
+
+def mean(temps):
+    return temps.mean(1) #average over frames
+
+def covs(temps):
+    pass
+
+def corr(temps):
+    pass
+
+def autocorrs(temps):
+    pass
+
+def autocovs(temps, n_tau=3, T=7):
+    trials, frames, comp  = temps.shape
+    cov_m = np.zeros([trials, n_tau, comp, comp])
+
+    for trial in range(trials):
+        for i_tau in range(n_tau):
+            cov_m[trial, i_tau, :, :] = np.tensordot(temps[trial, 0:T - n_tau + 1, :comp],
+                                                temps[trial, i_tau:T - n_tau + 1 + i_tau, :comp],
+                                                axes=(0, 0)) / float(T - n_tau)
+
+    return cov_m
+
---
+++ code/test_analysis.py
@@ -0,0 +1,56 @@
+import h5py
+import numpy
+import numpy as np
+import pandas as pd
+import pickle as pkl
+from pathlib import Path
+from load_task_data_as_pandas_df import extract_session_data_and_save
+
+from data import SVDData
+from measurements import mean
+from matplotlib import pyplot as plt
+
+import itertools
+
+plt_mean = False
+
+data_path = Path(__file__).parent.parent / Path('data')
+if not (data_path/'extracted_data.pkl').exists() :
+    # load behavior data
+    sessions = extract_session_data_and_save(root_paths=[data_path], mouse_ids=["GN06"], reextract=False)
+    with open( data_path / 'extracted_data.pkl', 'wb') as handle:
+        pkl.dump(sessions, handle)
+else:
+    # load saved data
+    with open( data_path / 'extracted_data.pkl', 'rb') as handle:
+        sessions = pkl.load(handle)
+    print("Loaded pickled data.")
+
+file_path = data_path / "GN06" / Path('2021-01-20_10-15-16/SVD_data/Vc.mat')
+f = h5py.File(file_path, 'r')
+
+frameCnt = np.array(f['frameCnt'])
+trial_starts = np.cumsum(frameCnt[:, 1])
+svd = SVDData( sessions, np.array(f["Vc"]), np.array(f["U"]), np.array(trial_starts) )
+
+trial_preselection = ((svd.n_targets == 6) & (svd.n_distractors == 0) &
+                      (svd.auto_reward == 0) & (svd.both_spouts == 1))
+svd_pre = svd[ :, trial_preselection ]
+
+
+#extract conds
+modal_keys = ['visual', 'tactile', 'vistact']
+modal_range = range(3)
+
+side_keys = ['right', 'left']
+side_range = range(2)
+
+
+
+svd_cond = [svd_pre[:,(svd_pre.modality == modal) & (svd_pre.target_side_left == side)] for modal, side in itertools.product(modal_range,side_range)]
+cond_keys =  list(itertools.product(modal_keys,side_keys))
+cond_keys_str = [f"{s}_{m}" for m, s in cond_keys]
+
+#cond_mean = mean(svd_cond[30:75,:]) #mean of stimulusframes
+
+print(svd_cond.shape)
