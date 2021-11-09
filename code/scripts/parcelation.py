ee61c9d7329e603e886fb04d6c0c05df3ee66651
DaminK <damin.kuehn@rwth-aachen.de>
2021-11-09 17:07:52 +0100

    outline other parcellations

--- code/scripts/parcelation.py
+++ code/scripts/parcelation.py
@@ -13,17 +13,21 @@ snakemake_tools.check_conf(snakemake, sections=["entry"])
 snakemake_tools.save_conf(snakemake, sections=["entry","parcelation"])
 
 
-
 def anatom():
     svd = DecompData.load(snakemake.input[0])
     anatomical = anatomical_parcellation(svd)
     anatomical.save(snakemake.output[0])
 
+def locaNMF():
+    pass
 
-parcelation = {'anatomical': anatom}
+
+parcelation = {'anatomical': anatom,
+               'locaNMF': locaNMF}
 
 
 parcelation[snakemake.wildcards['parcelation']]()
+
 
 # only supported in 3.10
 #match snakemake.wildcards['parcelation']:
