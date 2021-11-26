64dd9d4e99a9751b446f008cd2680ea6157ad6ba
Jonas Colve <jonas.colve@mailbox.org>
2021-11-26 18:32:22 +0100

    Moved feature and decoder options to branch_opts

--- workflow/scripts/feature.py
+++ workflow/scripts/feature.py
@@ -15,10 +15,18 @@ from features import Means, Raws, Covariances, AutoCovariances, Moup
 
 feature_dict = { "mean" : Means, "raw" : Raws, "covariance" : Covariances, "autocovariance" : AutoCovariances, "moup" :Moup }
 
-feature = snakemake.wildcards["feature"]
+param_dict = {
+        "mean"              : (lambda p : {}),
+        "raw"               : (lambda p : {}),
+        "covariance"        : (lambda p : {}),
+        "autocovariance"    : (lambda p : { "time_lag_range" : range(1,p["max_timelag"]+1) if "max_timelag" in p else p["timelags"] }),
+        "moup"              : (lambda p : p)}
+
+feature = snakemake.wildcards["feature"].split("_")[0]
+params = snakemake.params["params"]
 data = DecompData.load(snakemake.input[0])
 
-feat = feature_dict[feature].create(data, max_comps=config["max_components"])
+feat = feature_dict[feature].create(data, max_comps=config["max_components"], **param_dict[feature](params))
 
 
 feat.save(snakemake.output[0])
