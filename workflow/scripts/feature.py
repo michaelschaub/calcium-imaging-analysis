5f27f88d6f142d5031ab2f7004afb4ebf5590403
Jonas Colve <jonas.colve@mailbox.org>
2021-12-06 17:07:27 +0100

    Added comments to Snakefile, scripts/feature.py and _load_save_h5.py

--- workflow/scripts/feature.py
+++ workflow/scripts/feature.py
@@ -15,13 +15,17 @@ from features import Means, Raws, Covariances, AutoCovariances, Moup
 
 feature_dict = { "mean" : Means, "raw" : Raws, "covariance" : Covariances, "autocovariance" : AutoCovariances, "moup" :Moup }
 
+# Dictionary for converting parameters from workflow to parameters, that can be passed to feature creators
 param_dict = {
         "mean"              : (lambda p : {}),
         "raw"               : (lambda p : {}),
         "covariance"        : (lambda p : {}),
+        # convert parameter "max_timelag" to range up to that timelag, if "max_timelag" does not exist, pass "timelags" (iterable)
         "autocovariance"    : (lambda p : { "time_lag_range" : range(1,p["max_timelag"]+1) if "max_timelag" in p else p["timelags"] }),
+        # no conversion needed for Moup
         "moup"              : (lambda p : p)}
 
+# extract root name of feature (chop off all parameters)
 feature = snakemake.wildcards["feature"].split("_")[0]
 params = snakemake.params["params"]
 data = DecompData.load(snakemake.input[0])
