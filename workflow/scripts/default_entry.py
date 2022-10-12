f281da598ac100ff82e93035669a22b092d9003c
Damin <damin.kuehn@rwth-aachen.de>
2022-10-12 10:05:17 +0200

    Averages spatial components between all loaded session and transforms the temporals for each session accordingly = no longer a svd, but we only used it for dim reduction anyways =  error should be low. error approximation needed (without reconstructing org space)

--- workflow/scripts/default_entry.py
+++ workflow/scripts/default_entry.py
@@ -41,7 +41,7 @@ try:
         f = h5py.File(file_Vc, 'r')
 
         #Aligns spatials for each date with respective trans_params
-        alignend_U, align_plot = alignment.align_spatials_path(np.array(f["U"]),trans_path,plot_alignment_path=snakemake.output["align_plot"])
+        alignend_U, align_plot = alignment.align_spatials_path(np.array(f["U"]),trans_path,plot_alignment_path=snakemake.output["align_plot"]) #TODO export plot for each subject/session
         print(alignend_U.shape)
         U.append(alignend_U)
 
@@ -50,15 +50,64 @@ try:
         #assert np.array_equal(U[-1], U[0], equal_nan=True), "Combining different dates with different Compositions is not yet supported"
         # multiple dates cant have same spatials currently as they are independently created
 
-        trial_starts.append(np.cumsum(frameCnt[:-1, 1]) + start)
+        trial_starts.append(np.cumsum(frameCnt[:-1, 1]) + start) #TODO append will be deprecated, use concat
         start += Vc[-1].shape[0]
 
     trial_starts = np.concatenate( trial_starts )
+    #Vc = np.concatenate( Vc )
+
+
+    #
+
+    mean_U = np.mean(U,axis=0)
+
+    spat_n, w , h = mean_U.shape
+
+    mean_U = mean_U.reshape(spat_n,w * h)
+    print(mean_U.shape)
+
+    mean_U_inv = np.nan_to_num(np.linalg.pinv(np.nan_to_num(mean_U, nan=0.0)), nan=0.0)
+
+
+
+    error = np.zeros((len(U)))
+    print(mean_U_inv.shape)
+    print(len(Vc))
+    for i,V in enumerate(Vc):
+        print(V.shape)
+        print(V.dtype)
+        U[i] = U[i].reshape(spat_n,w * h)
+
+        V_transform = np.matmul(np.nan_to_num(U[i], nan=0.0), mean_U_inv)
+
+        Vc[i] = np.matmul(Vc[i], V_transform)
+        #V    spat_n**
+
+        #for t, frame in enumerate(V):
+
+        #    correct_frame = Vc[i][t]
+
+        #    Vc[i][t] = np.matmul(frame[np.newaxis,:] , V_transform)
+
+            #Vc[i][t] = frame * U[i] * mean_U_inv
+        #    error[i] += np.sum(np.absolute(Vc[i][t] - correct_frame))
+        #print(error[i])
+        print("after")
+        print(Vc[i].shape)
+        print(Vc[i].dtype)
+
+        print(np.isnan(np.sum(Vc[i])))
+
+        error[i] = np.linalg.norm(V_transform - np.eye(spat_n))
+
+
+
+    U = mean_U.reshape(spat_n,w,h) # U[0]
+    print(np.isnan(np.sum(U)))
+    print(error)
+
     Vc = np.concatenate( Vc )
 
-
-
-    U = U[0]
 
 
 
