765d6e1051201a64b0694542d01abe888a2e18e7
Jonas Colve <jonas.colve@mailbox.org>
2023-07-16 19:02:04 +0200

    Added first version of complete TrialData (appart from save/load)

--- ci_lib/data/decompdata.py
+++ ci_lib/data/decompdata.py
@@ -6,6 +6,7 @@ from typing import TypedDict, Optional, Union
 import numpy as np
 
 Index = Union[int,slice,np.ndarray[bool]]
+DecompDataList = Union['DecompData',list['DecompData']]
 Array = np.ndarray
 OptArray = Optional[Array]
 
@@ -55,46 +56,21 @@ class DecompData:
         self._mean = np.mean(self._temps,axis=0) if mean is None else mean
         self._stdev = np.std(self._temps,axis=0) if stdev is None else stdev
 
-    def copy(self, temporal_comps: OptArray = None,
-                    spatial_comps:  OptArray = None,
-                    spatial_labels: OptArray = None) -> 'DecompData':
-        '''Create a copy of this `DecompData` object optionally with some/all compoments replaced'''
-        if temporal_comps is None:
-            temporal_comps = np.copy(self._temps)
-            mean = np.copy(self._mean)
-            stdev = np.copy(self._stdev)
-        else:
-            mean = np.mean(temporal_comps,axis=0)
-            stdev = np.std(temporal_comps,axis=0)
-        if spatial_comps is None:
-            spatial_comps  = np.copy(self._spats)
-        if spatial_labels is None:
-            spatial_labels = np.copy(self._spat_labels)
-        return DecompData(temporal_comps, spatial_comps, spatial_labels=spatial_labels,
-                          mean=mean, stdev=stdev, logger=self._logger)
-
-    def concat(self, data:'DecompData', overwrite:bool=False):
-        '''concats trials from List of DecompData to this DecompData'''
-        if not isinstance(data, list):
-            data = [data]
-        if not overwrite:
-            data = [self, *data]
-        if len(data) > 1:
-            matching_spatials = (data[0].spatials==np.array(data[1:])).all(axis=0)
-            assert matching_spatials, "Spatials do not seem to match"
-        self._temps = np.concatenate([d.temporals for d in data], axis=0)
-
-    def __getitem__(self, key:Index) -> 'DecompData':
-        '''
-        Slices the DecompData object into a new DecompData object
-        the first key is applied to the temporal dimension
-        '''
-        return self.copy(temporal_comps=self._temps[key])
+### PROPERTIES
 
     @property
     def temporals(self) -> Array:
         '''Get temporal components of DecompData object, kept as one timeseries'''
         return self._temps
+
+    @property
+    def temporals_z_scored(self):
+        '''
+        Get z-score of temporal components of DecompData object, kept as one timeseries.
+        Z-score is calculated using the mean and standart deviation of the full dataset,
+        even after splitting the DecompData-object into conditions.
+        '''
+        return self._temps - self._mean * (1/self._stdev)
 
     @property
     def spatials(self) -> Array:
@@ -125,5 +101,47 @@ class DecompData:
     def t_max(self) -> int:
         '''The total length of temporal dimension'''
         return self._temps.shape[0]
+
+### FUNCTIONS
+
+    def copy(self, temporal_comps: OptArray = None,
+                    spatial_comps:  OptArray = None,
+                    spatial_labels: OptArray = None) -> 'DecompData':
+        '''Create a copy of this `DecompData` object optionally with some/all compoments replaced'''
+        if temporal_comps is None:
+            temporal_comps = np.copy(self._temps)
+            mean = np.copy(self._mean)
+            stdev = np.copy(self._stdev)
+        else:
+            mean = np.mean(temporal_comps,axis=0)
+            stdev = np.std(temporal_comps,axis=0)
+        if spatial_comps is None:
+            spatial_comps  = np.copy(self._spats)
+        if spatial_labels is None:
+            spatial_labels = np.copy(self._spat_labels)
+        return DecompData(temporal_comps, spatial_comps, spatial_labels=spatial_labels,
+                          mean=mean, stdev=stdev, logger=self._logger)
+
+    def concat(self, data:DecompDataList, overwrite:bool=False) -> 'DecompData':
+        '''
+        Concats trials from list of `DecompData` to this `DecompData`
+        WARNING: The `DecompData` objects in `data` should be considered modified and invalid after
+        '''
+        if not isinstance(data, list):
+            data = [data]
+        if not overwrite:
+            data = [self, *data]
+        if len(data) > 1:
+            matching_spatials = (data[0].spatials==np.array(data[1:])).all(axis=0)
+            assert matching_spatials, "Spatials do not seem to match"
+        self._temps = np.concatenate([d.temporals for d in data], axis=0)
+        return self
+
+    def __getitem__(self, key:Index) -> 'DecompData':
+        '''
+        Slices the DecompData object into a new DecompData object
+        the first key is applied to the temporal dimension
+        '''
+        return self.copy(temporal_comps=self._temps[key])
 
     #TODO implement pixel, save and load
--- ci_lib/data/trialdata.py
+++ ci_lib/data/trialdata.py
@@ -8,6 +8,9 @@ import pandas as pd
 
 LOGGER = logging.getLogger(__name__)
 
+MultiIndex = tuple
+Index = Union[np.ndarray[bool], int, slice, np.ndarray[int], MultiIndex, np.ndarray[MultiIndex]]
+TrialDataList = Union['TrialData',list['TrialData']]
 Event = tuple[
         Union[np.ndarray[int],np.ma.MaskedArray[int],np.ndarray[slice]],
         Optional[dict[str,np.ndarray[Any]]]
@@ -53,6 +56,25 @@ class TrialData:
         self._events    = events
         self._logger    = logger
 
+### PROPERTIES
+
+    @property
+    def n_trials(self) -> int:
+        ''' The number of trials'''
+        return self._dataframe.shape[0]
+
+    @property
+    def frame(self) -> pd.DataFrame:
+        '''The pandas DataFrame containing the trial data'''
+        return self._dataframe
+
+    @property
+    def events(self) -> Events:
+        '''The dictionary containing the trial events'''
+        return self._events
+
+### FUNCTIONS
+
     def copy(self) -> 'TrialData':
         '''Return a copy of this `TrialData` object'''
         #TODO add deep copy of events
@@ -69,10 +91,10 @@ class TrialData:
                 event_indx = np.array([slice(slc.first + offset, slc.second + offset)
                                         for slc in event_indx.flatten()]).reshape(event_indx_shape)
 
-    def concat(self, data:Union['TrialData',list['TrialData']], overwrite:bool=False):
+    def concat(self, data:TrialDataList, overwrite:bool=False) -> 'TrialData':
         '''
-        concats trials from list of `TrialData` to this `TrialData`
-        the `TrialData` objects in `data` should be considered modified and invalid after
+        Concats trials from list of `TrialData` to this `TrialData`
+        WARNING: The `TrialData` objects in `data` should be considered modified and invalid after
         '''
         if not isinstance(data, list):
             data = [data]
@@ -94,18 +116,108 @@ class TrialData:
         #TODO add treating of event kwargs
         self._events = { e_name : (np.concatenate(evnts[e_name][0] for evnts in dat_evnts),)
                         for e_name in evnt_keys}
+        return self
 
-    @property
-    def trials_n(self) -> int:
-        ''' The number of trials'''
-        return self._dataframe.shape[0]
+    def _events_by_indx(self, key):
+        #TODO add event kwargs
+        return {evnt_name : (evnt_indx[0][key],) for evnt_name, evnt_indx in self._events.items()}
 
-    @property
-    def frame(self) -> pd.DataFrame:
-        '''The pandas DataFrame containing the trial data'''
-        return self._dataframe
+    def __getitem__(self, key:Index) -> 'TrialData':
+        '''
+        Slices the `TrialData` object into a new `TrialData` object
+        If `key` is an integer, integer array/series or slice, trials will be selected \
+by dataframe integer position
+        If `key` is a tuple, tuple array/series or slice, trials will be selected via multiindex
+        If `key` is a bool array, ; it must have length `n_trials`
+        If `key` is a bool series, ; it must have the same index as `dataframe`
+        '''
 
-    @property
-    def events(self) -> Events:
-        '''The dictionary containing the trial events'''
-        return self._events
+        def cmp_slice_type(slc, typ):
+            '''Small helper function to compare the type of a slice to `t`'''
+            return ( (slc.start is None or isinstance(slc.start, typ))
+                and (slc.stop is not None and not isinstance(slc.start, typ)))
+
+        if isinstance(slice):
+            if cmp_slice_type(key, int):
+                dataframe = self._dataframe.iloc[key]
+                events = self._events_by_indx(key)
+            elif cmp_slice_type(key, tuple):
+                dataframe = self._dataframe.loc[key]
+                #TODO add event creation
+            else:
+                raise ValueError
+
+        # Treat `tuple` seperatly, since `pd.Series` would convert it to multiple singular keys
+        if isinstance(tuple):
+            key = pd.Series([key])
+        # Convert all other key(-arrays) into a `pandas.Series`
+        if not isinstance(key, pd.Series):
+            key = pd.Series(key)
+        # `key` is now a `pandas.Series`, determine accessing method from `key.dtype`
+        if key.dtype == int:
+            dataframe = self._dataframe.iloc[key]
+            events = self._events_by_indx(key)
+        elif key.dtype == tuple:
+            dataframe = self._dataframe.loc[key]
+            #TODO add event creation
+        elif key.dtype == bool:
+            assert len(key) == len(self._dataframe), ""
+            dataframe = self._dataframe.loc[key]
+            events = self._events_by_indx(key)
+        else:
+            raise ValueError
+
+        return TrialData(dataframe, events)
+
+    def get_conditional(self, conditions:dict) -> 'TrialData':
+        '''
+        returns slice of self, containing all trials, where the trial data given
+        by the keys of conditions match their corresponding values
+        '''
+        def check_attr(data_frame, attr, val):
+            if callable(val):
+                return val(getattr( data_frame, attr ))
+            return getattr( data_frame, attr ) == val
+
+        select = pd.Series(True, index=self._dataframe.index)
+        for attr, cond_val in conditions.items():
+            if isinstance(cond_val,list):
+                any_matching = pd.Series(False, index=self._dataframe.index)
+                for val in cond_val:
+                    any_matching = any_matching | check_attr(self._dataframe, attr, val)
+                select = select & any_matching
+            else:
+                select = select & check_attr(self._dataframe, attr, cond_val)
+        return self[select]
+
+    def dataset_from_sessions(self, sessions, dataset_id):
+        '''
+        Creates a TrialData object, only containing specified sessions and sets its dataset_column
+        to the specified dataset_id
+        sessions should be a list of dicts, each containing a `subject_id` string and a `datetime`
+        `numpy.datetime64` value
+        '''
+        subject_ids = [s['subject_id'] for s in sessions]
+        dates = [s['datetime'] for s in sessions]
+        def date_compare(date, date_comp):
+            return np.array(date, dtype=date.dtype) == date_comp
+        session_data = [self.get_conditional({'subject_id': subject_id})
+                                                    for subject_id in subject_ids]
+        session_data = [data.get_conditional({'date_time' : (
+                                lambda d, d_comp=date: date_compare(d,d_comp)) })
+                                                    for date, data in zip(dates, session_data)]
+        data = session_data[0]
+        data.concat(session_data, overwrite=True)
+        data.frame['dataset_id'] = dataset_id
+        return session_data[0]
+
+    def subsample(self,size,seed=None):
+        '''
+        Subsampling Trials to balance number of datapoints between different conditions
+        :param size: Number of trials to sample
+        :param seed: The seed for the rng
+        '''
+        rng = np.random.default_rng(seed)
+        select_n = rng.choice(self.trials_n,size=size,replace=False)
+        data = self[select_n]
+        return data
